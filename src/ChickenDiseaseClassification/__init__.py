88c7f02d790bfa46a9fd04974e3289459f741b2b
Nusri7 <321nusri@gmail.com>
2023-08-05 14:34:35 +0530

    created data ingestion pipeline

--- src/ChickenDiseaseClassification/__init__.py
+++ src/ChickenDiseaseClassification/__init__.py
@@ -5,6 +5,7 @@ import logging
 logging_str = "[%(asctime)s: %(levelname)s: %(module)s] %(message)s"
 log_dir = "logs"
 log_file_path = os.path.join(log_dir, "running_log.log")
+os.makedirs(log_dir, exist_ok=True)
 
 logging.basicConfig(filename=log_file_path,
                     level=logging.INFO, format=logging_str)
